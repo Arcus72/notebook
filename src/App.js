f0285d6502fc29554074d63a411449e1508eb7c9
Arcus72 <kimla.20034@gmail.com>
2021-03-30 01:01:08 +0200

    continue work on Note and App

--- src/App.js
+++ src/App.js
@@ -4,209 +4,207 @@ import NoteBook from './components/NoteBook/NoteBook';
 import FiltredNotes from './components/FiltredNotes/FiltredNotes';
 
 let listOfNotesFormula = [
-  {
-    id: 1,
-    title: 'Kosz',
-    context: 'Lorem ipsum',
-    color: '#28292c',
-    isPined: false,
-  },
-  {
-    id: 2,
-    title: 'Praca domowa',
-    context: '',
-    color: '#28292c',
-    isPined: true,
-  },
-  {
-    id: 3,
-    title: '',
-    context: 'Lorem ipsum',
-    color: '#5C2B29',
-    isPined: false,
-  },
+   {
+      id: 1,
+      title: 'Kosz',
+      context: 'Lorem ipsum',
+      color: '#28292c',
+      isPined: false,
+   },
+   {
+      id: 3,
+      title: '',
+      context: 'Lorem ipsum',
+      color: '#28292c',
+      isPined: false,
+   },
+   {
+      id: 2,
+      title: 'Praca domowa',
+      context: 'sdf sdf<br/>2<br/>3<br/>4<br/>5<br/>6<br/>7<br/>8<br/>9<br/><br/>',
+      color: '#28292c',
+      isPined: true,
+   },
+   {
+      id: 10,
+      title: 'Dokończ strone',
+      context: '',
+      color: '#5C2B29',
+      isPined: false,
+   },
+   {
+      id: 4,
+      title: 'React',
+      context: '<br/>2<br/>3<br/>4<br/>5<br/>6<br/>7<br/>8<br/>9<br/><br/>',
+      color: '#5C2B29',
+      isPined: false,
+   },
+   {
+      id: 5,
+      title: 'dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd',
+      context: '',
+      color: '#5C2B29',
+      isPined: false,
+   },
+   {
+      id: 6,
+      title: '',
+      context: 'ddddddddddddddddddddddddddddddddddddddddddddddddd',
+      color: '#5C2B29',
+      isPined: false,
+   },
 ];
 
 const colorList = [
-  {
-    name: 'Czarny',
-    value: '#28292c',
-  },
-  {
-    name: 'Czerwony',
-    value: '#5C2B29',
-  },
-  {
-    name: 'Pomarańcz',
-    value: '#614A19',
-  },
-  {
-    name: 'Żółty',
-    value: '#635D19',
-  },
-  {
-    name: 'Zielony',
-    value: '#345920',
-  },
-  {
-    name: 'Morski',
-    value: '#16504B',
-  },
-  {
-    name: 'Niebieski',
-    value: '#2D555E',
-  },
-  {
-    name: 'Ciemny Niebieski',
-    value: '#1E3A5F',
-  },
-  {
-    name: 'Fiolet',
-    value: '#42275E',
-  },
-  {
-    name: 'Róż',
-    value: '#5B2245',
-  },
-  {
-    name: 'Brąz',
-    value: '#442F19',
-  },
-  {
-    name: 'Szary',
-    value: '#3C3F43',
-  },
+   {
+      name: 'Czarny',
+      value: '#28292c',
+   },
+   {
+      name: 'Czerwony',
+      value: '#5C2B29',
+   },
+   {
+      name: 'Pomarańcz',
+      value: '#614A19',
+   },
+   {
+      name: 'Żółty',
+      value: '#635D19',
+   },
+   {
+      name: 'Zielony',
+      value: '#345920',
+   },
+   {
+      name: 'Morski',
+      value: '#16504B',
+   },
+   {
+      name: 'Niebieski',
+      value: '#2D555E',
+   },
+   {
+      name: 'Ciemny Niebieski',
+      value: '#1E3A5F',
+   },
+   {
+      name: 'Fiolet',
+      value: '#42275E',
+   },
+   {
+      name: 'Róż',
+      value: '#5B2245',
+   },
+   {
+      name: 'Brąz',
+      value: '#442F19',
+   },
+   {
+      name: 'Szary',
+      value: '#3C3F43',
+   },
 ];
 
 const getFirstFreeId = (list) => {
-  //! Czy tyle kodu jest potrzebne
-  const idList = list.map((item) => item.id);
-  idList.sort((a, b) => {
-    return a - b;
-  });
-
-  if (idList[0] !== 1 && idList.length === 0) return 1;
-  let result;
-  let isIdFind = false;
-
-  idList.forEach((id, index, arr) => {
-    if (isIdFind) return 1;
-
-    if (arr[index + 1] === undefined) {
-      result = id + 1;
-      return 1;
-    }
-
-    if (arr[index + 1] - id !== 1) {
-      isIdFind = true;
-      result = id + 1;
-      return 1;
-    }
-  });
-  return result;
+   const idList = list.map((item) => item.id);
+   for (let i = 1; true; i++) {
+      if (idList.indexOf(i) === -1) return i;
+   }
 };
 
 const reduce = (state, action) => {
-  const listCopy = [...state];
-  let noteIndex;
-  switch (action.type) {
-    case 'setNewNote':
-      console.log('setNewNote');
+   const listCopy = [...state];
+   let noteIndex;
+   let noteCopy;
+   switch (action.type) {
+      case 'setNewNote':
+         console.log('setNewNote');
 
-      return state;
-    case 'edit':
-      noteIndex = listCopy.findIndex((item) => {
-        return item.id === action.value.id;
-      });
-      listCopy[noteIndex] = action.value.newVersion;
+         return state;
+      case 'edit':
+         noteIndex = listCopy.findIndex((item) => item.id === action.value.id);
+         listCopy[noteIndex] = action.value.newVersion;
+         return listCopy;
 
-      return listCopy;
+      case 'changePinStatus':
+         noteIndex = listCopy.findIndex((item) => item.id === action.value.id);
 
-    case 'delete':
-      console.log('delete');
+         listCopy.splice(noteIndex, 1);
 
-      noteIndex = listCopy.findIndex((item) => {
-        return item.id === action.value;
-      });
-      listCopy.splice(noteIndex, 1);
-      return listCopy;
+         return [action.value.newVersion, ...listCopy];
 
-    case 'copy':
-      const index = getFirstFreeId(state);
-      noteIndex = listCopy.findIndex((item) => {
-        return item.id === action.value;
-      });
-      const noteCopy = { ...state[noteIndex] };
-      noteCopy.id = index;
+      case 'delete':
+         noteIndex = listCopy.findIndex((item) => item.id === action.value.id);
+         listCopy.splice(noteIndex, 1);
+         return listCopy;
 
-      return [...state, noteCopy];
+      case 'copy':
+         const index = getFirstFreeId(state);
+         noteIndex = listCopy.findIndex((item) => item.id === action.value.id);
+         noteCopy = { ...state[noteIndex] };
+         noteCopy.id = index;
 
-    default:
-      return state;
-  }
+         return [...state, noteCopy];
+
+      default:
+         return state;
+   }
 };
 
 export const NotesModifierContext = createContext();
 function App() {
-  const inputHolder = useRef();
-  const input = useRef();
-  const [isSearching, setIsSearching] = useState(false);
-  const [listOfNotes, notesModifier] = useReducer(reduce, listOfNotesFormula);
+   const inputHolder = useRef();
+   const input = useRef();
+   const [isSearching, setIsSearching] = useState(false);
+   const [listOfNotes, notesModifier] = useReducer(reduce, listOfNotesFormula);
 
-  const changeInputHolderColor = (action) => {
-    //! zmień nazwe funkcji
-    if (action.type === 'focus') {
-      console.log('Focus');
-      inputHolder.current.classList.add('App__inputHolder--focus');
-      setIsSearching(true);
-      return;
-    }
-    inputHolder.current.classList.remove('App__inputHolder--focus');
-  };
+   const changeInputHolderColor = (action) => {
+      if (action.type === 'focus') {
+         inputHolder.current.classList.add('App__inputHolder--focus');
+         setIsSearching(true);
+         return 1;
+      }
+      inputHolder.current.classList.remove('App__inputHolder--focus');
+   };
 
-  return (
-    <div className='App'>
-      <NotesModifierContext.Provider
-        value={{
-          colorList: colorList,
-          notesModifier: notesModifier,
-        }}
-      >
-        <div className='App__banner'>
-          <header className='App__title'>NOTATNIK</header>
-          <div ref={inputHolder} className='App__inputHolder'>
-            <i className='App__searchIcon fas fa-search'></i>
-            <input
-              ref={input}
-              placeholder='Szukaj'
-              onFocus={() =>
-                changeInputHolderColor({ type: 'focus', value: null })
-              }
-              onBlur={(e) => changeInputHolderColor({ type: 'blur', value: e })}
-              className='App__input'
-              type='text'
-            />
-            {isSearching && (
-              <i
-                className='App__crossIcon fas fa-times'
-                onClick={() => {
-                  setIsSearching(false);
-                  input.current.value = '';
-                }}
-              ></i>
-            )}
-          </div>
-        </div>
-        <div className='App__line'></div>
+   return (
+      <div className='App'>
+         <NotesModifierContext.Provider
+            value={{
+               colorList: colorList,
+               notesModifier: notesModifier,
+            }}
+         >
+            <div className='App__banner'>
+               <header className='App__title'>NOTATNIK</header>
+               <div ref={inputHolder} className='App__inputHolder'>
+                  <i className='App__searchIcon fas fa-search'></i>
+                  <input
+                     ref={input}
+                     placeholder='Szukaj'
+                     onFocus={() => changeInputHolderColor({ type: 'focus', value: null })}
+                     onBlur={(e) => changeInputHolderColor({ type: 'blur', value: e })}
+                     className='App__input'
+                     type='text'
+                  />
+                  {isSearching && (
+                     <i
+                        className='App__crossIcon fas fa-times'
+                        onClick={() => {
+                           setIsSearching(false);
+                           input.current.value = '';
+                        }}
+                     ></i>
+                  )}
+               </div>
+            </div>
+            <div className='App__line'></div>
 
-        {isSearching ? (
-          <FiltredNotes list={listOfNotes} />
-        ) : (
-          <NoteBook notesModifier={notesModifier} list={listOfNotes} />
-        )}
-      </NotesModifierContext.Provider>
-    </div>
-  );
+            {isSearching ? <FiltredNotes list={listOfNotes} /> : <NoteBook notesModifier={notesModifier} list={listOfNotes} />}
+         </NotesModifierContext.Provider>
+      </div>
+   );
 }
 
 export default App;
--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -3,76 +3,79 @@ import './index.scss';
 import { NotesModifierContext } from 'src/App';
 
 function Note({ data }) {
-  const { notesModifier, colorList } = useContext(NotesModifierContext);
+   const { notesModifier, colorList } = useContext(NotesModifierContext);
+   const [isOverflow, setIsOverflow] = React.useState(false);
+   const pinIconClass = `Note__pinIcon ${data.isPined ? 'Note__pinIcon--pined' : ''}`;
+   const contextValueRef = React.useRef();
 
-  const pinIconClass = `Note__pinIcon ${
-    data.isPined ? 'Note__pinIcon--pined' : ''
-  }`;
+   React.useEffect(() => {
+      //TODO: to samo dla title
+      let i = contextValueRef.current.scrollHeight - contextValueRef.current.getBoundingClientRect().height;
+      i = Math.round(i);
+      console.log(i);
+      if (i !== 0) setIsOverflow(true);
+   }, []);
 
-  const changeIsPinedValue = () => {
-    const dataCopy = data;
-    data.isPined = !dataCopy.isPined;
-    notesModifier({
-      type: 'edit',
-      value: { id: data.id, newVersion: dataCopy },
-    });
-  };
-
-  const changeNoteColor = (color) => {
-    console.log(color);
-    if (color !== data.color) {
-      let dataCopy = data;
-      dataCopy.color = color;
+   const changeIsPinedValue = () => {
+      const dataCopy = data;
+      data.isPined = !dataCopy.isPined;
       notesModifier({
-        type: 'edit',
-        value: { id: data.id, newVersion: dataCopy },
+         type: 'changePinStatus',
+         value: { id: data.id, newVersion: dataCopy },
       });
-    }
-  };
+   };
 
-  //! po najechaniu pojawia się nazwa
-  const colorPalette = colorList.map((item, index) => (
-    <span
-      key={index}
-      className={`Note__color ${
-        item.value === data.color ? 'Note__color--current' : ''
-      }`}
-      onClick={() => changeNoteColor(item.value)}
-      style={{ background: item.value }}
-    ></span>
-  ));
+   const changeNoteColor = (color) => {
+      if (color !== data.color) {
+         let dataCopy = data;
+         dataCopy.color = color;
+         notesModifier({
+            type: 'edit',
+            value: { id: data.id, newVersion: dataCopy },
+         });
+      }
+   };
 
-  return (
-    <div style={{ background: data.color }} className='Note'>
-      <header className='Note__title'>
-        <div className='Note__titleValue'>{data.title}</div>
-        {/* ! jeśli jest puste to znika  */}
-        <div onClick={changeIsPinedValue} className={pinIconClass}>
-          <i className='fas fa-map-pin'></i>
-        </div>
-      </header>
-      <div className='Note__context'>
-        {data.context ? data.context : <br />}
+   const colorPalette = colorList.map((item, index) => (
+      <span
+         title={item.name}
+         key={index}
+         className={`Note__color ${item.value === data.color ? 'Note__color--current' : ''}`}
+         onClick={() => changeNoteColor(item.value)}
+         style={{ background: item.value }}
+      ></span>
+   ));
+   //TODO: anime on enter / leave oraz anime move
+
+   //NOTE: Może title i context w jednym div i wtedy ...
+   return (
+      <div style={{ background: data.color }} className='Note'>
+         <header className='Note__title'>
+            {/* TODO: if(title === "") 2*icon i przełączanie  */}
+            <div className='Note__titleValue'>
+               <div onClick={changeIsPinedValue} className={pinIconClass}>
+                  <i className='fas fa-map-pin'></i>
+               </div>
+               {data.title}
+            </div>
+         </header>
+
+         <div
+            className='Note__context'
+            ref={contextValueRef}
+            dangerouslySetInnerHTML={{ __html: data.context ? data.context : '<br />' }}
+         ></div>
+         {isOverflow && <div className='Note__continuationDots'>...</div>}
+         <div className='Note__options'>
+            <i title='usuń' onClick={() => notesModifier({ type: 'delete', value: data.id })} className='fas fa-trash'></i>
+            <i title='kopiuj' onClick={() => notesModifier({ type: 'copy', value: data.id })} className='fas fa-copy'></i>
+            <span className='Note__palette'>
+               <i className='fas fa-palette'></i>
+               <div className='Note__colorContainer'>{colorPalette}</div>
+            </span>
+         </div>
       </div>
-      <div className='Note__options'>
-        <i
-          onClick={() => notesModifier({ type: 'delete', value: data.id })}
-          className='fas fa-trash'
-        ></i>
-        <i
-          onClick={() => notesModifier({ type: 'copy', value: data.id })}
-          className='fas fa-copy'
-        ></i>
-        <span className='Note__palette'>
-          <i
-            // onClick={}
-            className='fas fa-palette'
-          ></i>
-          <div className='Note__colorContainer'> {colorPalette}</div>
-        </span>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default Note;
